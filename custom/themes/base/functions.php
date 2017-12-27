d458423ea5452e75eb6dc554bcc668b36b4a8a54
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-12-27 16:06:51 -0500

    Removes Project CPT. Updates Record CPT

---
+++ custom/themes/base/functions.php
@@ -0,0 +1,11 @@
+<?php
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'menus' );
+//Load custom functions
+require_once('includes/functions/enqueue-style.php');
+require_once('includes/functions/enqueue-script.php');
+// Load MetaBox functions
+require_once('includes/functions/metabox/cpt-page.php');
+require_once('includes/functions/metabox/cpt-post.php');
+require_once('includes/functions/metabox/cpt-record.php');
+?>
